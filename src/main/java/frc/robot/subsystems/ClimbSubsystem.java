48c1a6f248358cab61f13db9c133cfa9c472e92c
LCMadsen <bluejacketprogramming@gmail.com>
2025-01-22 16:18:58 -0600

    gaboola

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class ClimbSubsystem {
+<<<<<<< Updated upstream
   
   SparkMax climb1 = new SparkMax(0, MotorType.kBrushless);
   SparkMax climb2 = new SparkMax(0, MotorType.kBrushless);
@@ -26,4 +29,35 @@ public class ClimbSubsystem {
   
     climb1.configure(sparkMaxConfig, null, null);    
   }
+=======
+    
+    SparkMax climb1 = new SparkMax(0, MotorType.kBrushless);
+    SparkMax climb2 = new SparkMax(0, MotorType.kBrushless);
+    SparkMaxConfig sparkMaxConfig = new SparkMaxConfig();
+    SparkClosedLoopController loopController = climb1.getClosedLoopController();
+    double kP = 800000;
+    double kI = 69420.1;
+    double kD = 8008;
+    double kIz = 1337;
+    double kFF = 666;
+    double extMaxOutput = -2;
+    double extMinOutput = 2;
+
+    public void init(){
+        sparkMaxConfig.closedLoop
+            .p(kP)
+            .i(kI)
+            .d(kD)
+            .outputRange(extMinOutput, extMaxOutput);
+    
+        climb1.configure(sparkMaxConfig, null, null);        
+    }
+
+
+    public void periodic(){
+
+        loopController.setReference(400, ControlType.kPosition );
+        
+    }
+>>>>>>> Stashed changes
 }
---
+++ src/main/java/frc/robot/subsystems/exampleSubsystem.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class exampleSubsystem {
+    SparkMax testmotor = new SparkMax(9, MotorType.kBrushless);
+
+
+
+
+    // PID SETUP STUFF
+    public void init(){
+
+        // SET YOUR CONSTANTS
+
+    }
+
+    // THIS IS WHERE YOU MAKE THINGS MOVE
+    public void makeItMove(){
+
+        // SET YOUR REFERENCE
+        
+    }
+
+
+}
