762b6a341349793d886201ce28286aeabc23f0e5
Chuckles <nthill2011@outlook.com>
2025-01-25 11:23:55 -0600

    Put in lasercan code

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -7,9 +7,12 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorSubsystem {
+  private LaserCan lc;
 
   SparkMax leftEl = new SparkMax(30, MotorType.kBrushless);
   SparkMax rightEl = new SparkMax(36, MotorType.kBrushless); //right follows Left
@@ -38,10 +41,33 @@ public class ElevatorSubsystem {
     // leftEl.configure(sparkMaxConfig, null, null);
 
   }
+  //lasercan
+  public void laserInit() {
+    lc = new LaserCan(199);
+    // Optionally initialise the settings of the LaserCAN, if you haven't already done so in GrappleHook
+    try {
+      lc.setRangingMode(LaserCan.RangingMode.SHORT);
+      lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+      lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+    } catch (ConfigurationFailedException e) {
+      System.out.println("Configuration failed! " + e);
+    }
+  }
+
+  public void laserPeriodic() {
+    LaserCan.Measurement measurement = lc.getMeasurement();
+    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+      System.out.println("The target is " + measurement.distance_mm + "mm away!");
+    } else {
+      System.out.println("Oh no! The target is out of range, or we can't get a reliable measurement!");
+      // You can still use distance_mm in here, if you're ok tolerating a clamped value or an unreliable measurement.
+    }
+  }
 
   public void periodic(){
   elencoderPos = leftelEncoder.getPosition();
   SmartDashboard.putNumber("ElEncoder", elencoderPos);
+  laserPeriodic();
   }
 
   public void goElevate(){
