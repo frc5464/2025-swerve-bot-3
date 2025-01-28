f13d74154922c9394da5ebb067d6a315ab74a367
LCMadsen <bluejacketprogramming@gmail.com>
2025-01-28 17:41:06 -0600

    gaboola

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,16 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ArmSubsystem {
@@ -38,8 +36,34 @@ public class ArmSubsystem {
     slot0Configs.kI = 0;
     slot0Configs.kD = 0.1;
 
-    
-    //armEncoder = armRot.getEncoder();
+    slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+    slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kI = 0; // no output for integrated error
+    slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+    armRot.getConfigurator().apply(slot0Configs);
+
+    // Trapezoid profile with max velocity 80 rps, max accel 160 rps/s
+  final TrapezoidProfile m_profile = new TrapezoidProfile(
+   new TrapezoidProfile.Constraints(80, 160)
+  );
+  // Final target of 200 rot, 0 rps
+  TrapezoidProfile.State m_goal = new TrapezoidProfile.State(200, 0);
+  TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
+
+  // create a position closed-loop request, voltage output, slot 0 configs
+  final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+
+  // calculate the next profile setpoint
+  m_setpoint = m_profile.calculate(0.020, m_setpoint, m_goal);
+
+  // send the request to the device
+  m_request.Position = m_setpoint.position;
+  m_request.Velocity = m_setpoint.velocity;
+  armRot.setControl(m_request);
+
+  //armEncoder = armRot.getEncoder();
   //Encoderstuff
   // sparkMaxConfig.closedLoop
   //   .p(kP)
