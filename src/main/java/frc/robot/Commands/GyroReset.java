9a9acf5717ed62cb62fa84799a5b0083a5d4e158
Team5464 <bluejacketprogramming@gmail.com>
2025-03-10 17:27:05 -0500

    Gyro Fixin

--- src/main/java/frc/robot/Commands/GyroReset.java
+++ src/main/java/frc/robot/Commands/GyroReset.java
@@ -4,18 +4,17 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class GyroReset extends Command{
-    private final Runnable reset;
+    // private final Runnable reset;
     
 
     public GyroReset(SwerveSubsystem swerveSubsystem) {
-        reset = () -> {
-            swerveSubsystem.zeroGyro();
-        };
+        swerveSubsystem.zeroGyro();
+    
     }
 
     @Override
     public void initialize() {
-        reset.run();
+        // reset.run();
     }
 
     @Override
